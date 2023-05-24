48a81085744f50087664b6d01e5ee5aed2f78206
Eduard Luhtonen <luhtonen@gmail.com>
2023-05-24 15:57:25 +0000

    sample of actix web usage

---
+++ calc/src/lib.rs
@@ -0,0 +1,21 @@
+// build calculator functions
+
+// add two number
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// subtract two number
+pub fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+// multiply two number
+pub fn multiply(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+// devide two number
+pub fn devide(a: i32, b: i32) -> i32 {
+    a / b
+}
